640d0e5ea35bca9f4761c8aaa4a8e2789f27e09d
Sahil Muthoo <sahil.muthoo@gmail.com>
2013-10-02 23:06:41 +0530

    Add file name completion and a better prompt

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,8 +1,14 @@
+module Shelter
+  FILE_NAME_COMPLETER = ->(input) do
+    if File.directory?(input) && !input.end_with?('/')
+      Dir["#{input}/*"]
+    else
+      Dir["#{input}*"]
+    end
+  end
+end
+
 require 'readline'
 require 'shellwords'
-
-require 'shelter/shell'
 require 'shelter/version'
-
-module Shelter
-end
+require 'shelter/shell'
